d87e229289fd6ebcad4f6cf57a71c844c5b7a2e0
felix <55251566+johnfelix34@users.noreply.github.com>
2020-01-29 13:46:21 +0530

    quiz program ## To make a commit, type your commit message and press CTRL-ENTER. ## To cancel the commit, close the window. To sign off on the commit, ## press CTRL-S. ## ## You may also reference or close a GitHub issue with this commit. ## To do so, type `#` followed by the `tab` key.  You will be shown a ## list of issues related to the current repo.  You may also type ## `owner/repo#` plus the `tab` key to reference an issue in a ## different GitHub repo.
     client/index.php | 8 +++++++-
     1 file changed, 7 insertions(+), 1 deletion(-)

--- client/index.php
+++ client/index.php
@@ -2,12 +2,18 @@
 <html>
 <head>
 	<title>Quiz Program</title>
+  <link href="https://unpkg.com/material-components-web@latest/dist/material-components-web.min.css" rel="stylesheet">
+  <script src="https://unpkg.com/material-components-web@latest/dist/material-components-web.min.js"></script>
 </head>
 <body>
 	<nav>
 		<label>title</label>
 		<label>timer</label>
 		<label>username</label>
+		<button class="mdc-button" >
+  <div class="mdc-button__ripple"></div>
+  <span class="mdc-button__label">Button</span>
+</button>
 		<div id = "dropdown">
 			<label id = "lbluname"></label>
 			<label id = "lbluid"></label>
@@ -23,7 +29,7 @@
 	<div id = "res">
 		<label id = "lblqa">Questions Answered <bdi id = "qanum"></bdi></label>
 		<label id = "lblmark">Total Mark <bdi id = "marknum"></bdi></label>
-	</div>
+	</div>-->
 <footer></footer>
 </body>
 </html>
